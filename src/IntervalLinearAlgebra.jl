f53197a705a131a7e7cad0dca326a8d878113ccb
lucaferranti <lucaa.ferranti@gmail.com>
2021-06-15 18:08:48 +0300

    added functionality to solve linear system with oettli-präger theorem
    updated oettli solver
    
    added tests for oettli
    
    do not check lengths after paving in tests
    
    do paving inside oettli-präger solver

--- src/IntervalLinearAlgebra.jl
+++ src/IntervalLinearAlgebra.jl
@@ -1,14 +1,16 @@
 module IntervalLinearAlgebra
 
 # Write your package code here.
-using LinearAlgebra, IntervalArithmetic, StaticArrays
+using LinearAlgebra, IntervalArithmetic, StaticArrays, IntervalConstraintProgramming
 
 export
     Krawczyk, Jacobi, GaussSeidel, GaussElimination, HansenBliekRohn,
-    solve, enclose, precondition,
-    comparison_matrix, interval_norm, interval_isapprox
+    solve, enclose, precondition, oettli,
+    comparison_matrix, interval_norm, interval_isapprox,
+    comparison_matrix, interval_norm
 
 
 include("solvers/solvers.jl")
 include("utils.jl")
+include("solvers/oettli.jl")
 end
