652496549b9e063b2a8f8193f945a057ffdd9f22
lucaferranti <lucaa.ferranti@gmail.com>
2021-07-03 13:42:17 +0300

    added examples to classify docstrings

--- src/classify.jl
+++ src/classify.jl
@@ -1,11 +1,29 @@
 # Routines to classify interval matrices
 
 """
-    is_strongly_regular(A)
+    is_strongly_regular(A::AbstractMatrix{T}) where {T<:Interval}
 
-Tests whether an interval matrix A is strongly regular.
-An interval matrix `A` is strongly regular if Ac⁻¹A is regular.
-For more details see section 4.6 of [[HOR19]](@ref)
+Tests whether an interval matrix ``A`` is strongly regular, i.e. if if ``A_c^{-1}A`` is
+an H-matrix. For more details see section 4.6 of [[HOR19]](@ref).
+
+### Examples
+```jldoctest
+julia> A = [2..4 -2..1; -1..2 2..4]
+2×2 Matrix{Interval{Float64}}:
+  [2, 4]  [-2, 1]
+ [-1, 2]   [2, 4]
+
+julia> is_strongly_regular(A)
+true
+
+julia> A = [0..2 1..1;-1.. -1 0..2]
+2×2 Matrix{Interval{Float64}}:
+   [0, 2]  [1, 1]
+ [-1, -1]  [0, 2]
+
+julia> is_strongly_regular(A)
+false
+```
 """
 function is_strongly_regular(A)
     m, n = size(A)
@@ -17,12 +35,31 @@ function is_strongly_regular(A)
 end
 
 """
-    is_H_matrix(A)
+    is_H_matrix(A::AbstractMatrix{T}) where {T<:Interval}
 
-Tests whether an interval matrix A is an H-matrix, by testing that ⟨A⟩⁻¹e>0,
-where e=[1, 1, …, 1]ᵀ. Note that in practice it tests that an _approximation_ of
-⟨A⟩⁻¹e satisfies the condition.
-For more details see section 4.4 of [[HOR19]](@ref)
+Tests whether an interval matrix A is an H-matrix, by testing that ``⟨A⟩^{-1}e>0``,
+where ``e=[1, 1, …, 1]ᵀ``. Note that in practice it tests that a
+_floating point approximation_ of ``⟨A⟩^{-1}e`` satisfies the condition.
+For more details see section 4.4 of [[HOR19]](@ref).
+
+### Examples
+```jldoctest
+julia> A = [2..4 -1..1; -1..1 2..4]
+2×2 Matrix{Interval{Float64}}:
+  [2, 4]  [-1, 1]
+ [-1, 1]   [2, 4]
+
+julia> is_H_matrix(A)
+true
+
+julia> A = [2..4 -2..1; -1..2 2..4]
+2×2 Matrix{Interval{Float64}}:
+  [2, 4]  [-2, 1]
+ [-1, 2]   [2, 4]
+
+julia> is_H_matrix(A)
+false
+```
 """
 function is_H_matrix(A)
     m, n = size(A)
@@ -35,11 +72,30 @@ end
 
 
 """
-    is_strictly_diagonally_dominant(A)
+    is_strictly_diagonally_dominant(A::AbstractMatrix{T}) where {T<:Interval}
 
-Checks whether an interval matrix A is stictly diagonally dominant, that is
-if mig(Aᵢᵢ) > ∑_(k ≠ i) mag(Aᵢₖ) for i=1,…,n.
-For more details see section 4.5 of [[HOR19]](@ref)
+Checks whether an interval matrix ``A`` is stictly diagonally dominant, that is
+if ``mig(Aᵢᵢ) > ∑_{k ≠ i} mag(Aᵢₖ)`` for ``i=1,…,n``.
+For more details see section 4.5 of [[HOR19]](@ref).
+
+### Examples
+```jldoctest
+julia> A = [2..4 -1..1; -1..1 2..4]
+2×2 Matrix{Interval{Float64}}:
+  [2, 4]  [-1, 1]
+ [-1, 1]   [2, 4]
+
+julia> is_strictly_diagonally_dominant(A)
+true
+
+julia> A = [2..4 -2..1; -1..2 2..4]
+2×2 Matrix{Interval{Float64}}:
+  [2, 4]  [-2, 1]
+ [-1, 2]   [2, 4]
+
+julia> is_strictly_diagonally_dominant(A)
+false
+```
 """
 function is_strictly_diagonally_dominant(A)
     m, n = size(A)
@@ -56,8 +112,27 @@ end
 """
     is_Z_matrix(A)
 
-Checks whether the interval matrix A is a Z-matrix, that is whether Aᵢⱼ≤0 for all i≠j.
-For more details see section 4.2 of [[HOR19]](@ref)
+Checks whether the interval matrix ``A`` is a Z-matrix, that is whether ``Aᵢⱼ≤0`` for all
+``i≠j``. For more details see section 4.2 of [[HOR19]](@ref).
+
+### Examples
+```jldoctest
+julia> A = [2..4 -2.. -1; -2.. -1 2..4]
+2×2 Matrix{Interval{Float64}}:
+   [2, 4]  [-2, -1]
+ [-2, -1]    [2, 4]
+
+julia> is_Z_matrix(A)
+true
+
+julia> A = [2..4 -2..1; -1..2 2..4]
+2×2 Matrix{Interval{Float64}}:
+  [2, 4]  [-2, 1]
+ [-1, 2]   [2, 4]
+
+julia> is_Z_matrix(A)
+false
+```
 """
 function is_Z_matrix(A)
 
@@ -75,10 +150,29 @@ function is_Z_matrix(A)
 end
 
 """
-    is_M_matrix(A)
+    is_M_matrix(A::AbstractMatrix{T}) where {T}
 
-Checks whether the interval matrix A is an M-matrix, that is a Z-matrix with non-negative inverse.
-For more details see section 4.2 of [[HOR19]](@ref)
+Checks whether the interval matrix ``A`` is an M-matrix, that is a Z-matrix with
+non-negative inverse. For more details see section 4.2 of [[HOR19]](@ref).
+
+### Examples
+```jldoctest
+julia> A = [2..2 -1..0; -1..0 2..2]
+2×2 Matrix{Interval{Float64}}:
+  [2, 2]  [-1, 0]
+ [-1, 0]   [2, 2]
+
+julia> is_M_matrix(A)
+true
+
+julia> A = [2..4 -2..1; -1..2 2..4]
+2×2 Matrix{Interval{Float64}}:
+  [2, 4]  [-2, 1]
+ [-1, 2]   [2, 4]
+
+julia> is_M_matrix(A)
+false
+```
 """
 function is_M_matrix(A)
 
