123f0d51e031d21e2b866b23944101176fac0a74
lucaferranti <49938764+lucaferranti@users.noreply.github.com>
2021-07-19 10:19:00 +0300

    verified solution of real linear solver (#25)
    * added first implementation of verified_linear_solver
    
    added test for verified linear solver
    
    added version for interval inputs
    
    update tests
    
    updated docstring
    
    remove extra line in tests
    
    changed name to epsilon_inflation
    
    allow b to be also a matrix
    
    small fixes
    
    added note to docstring to clarify purpose of this function
    
    added algorithm description
    
    avoid duplicate code
    
    added test for bigfloats
    
    updated mid
    
    * fixed tests

--- src/IntervalLinearAlgebra.jl
+++ src/IntervalLinearAlgebra.jl
@@ -3,6 +3,7 @@ module IntervalLinearAlgebra
 using StaticArrays, Requires, Reexport
 
 import CommonSolve: solve
+import IntervalArithmetic: mid
 
 function  __init__()
     @require IntervalConstraintProgramming = "138f1668-1576-5ad7-91b9-7425abbf3153" include("solvers/oettli.jl")
@@ -14,7 +15,7 @@ end
 export
     LinearKrawczyk, Jacobi, GaussSeidel, GaussianElimination, HansenBliekRohn, NonLinearOettliPrager, LinearOettliPrager,
     NoPrecondition, InverseMidpoint, InverseDiagonalMidpoint,
-    solve, enclose,
+    solve, enclose, epsilon_inflation,
     comparison_matrix, interval_norm, interval_isapprox, list_orthants,
     is_H_matrix, is_strongly_regular, is_strictly_diagonally_dominant, is_Z_matrix, is_M_matrix,
     rref
@@ -23,6 +24,7 @@ export
 include("solvers/hull.jl")
 include("solvers/precondition.jl")
 include("solvers/solve.jl")
+include("solvers/verify.jl")
 
 include("utils.jl")
 include("classify.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using IntervalLinearAlgebra, StaticArrays, IntervalConstraintProgramming, LazySets
 using Test
 
+const IA = IntervalArithmetic
+
 @testset "IntervalLinearAlgebra.jl" begin
 
     @testset "Utils" begin
@@ -121,5 +123,44 @@ using Test
 
         A2 = fill(0..0, 2, 2)
         @test_throws ArgumentError rref(A2)
+    end
+
+    @testset "Verified linear solver" begin
+
+        n = 4
+        Arat = reshape(1 .//(1:n^2), n, n)
+        brat = Arat*fill(1//1, n)
+        Afloat = float(Arat)
+        bfloat = float(brat)
+
+        x, cert = epsilon_inflation(Afloat, bfloat)
+
+        @test all(ones(n) .∈ x)
+        @test cert
+
+        Ain = convert.(IA.Interval{Float64}, IA.Interval.(Arat, Arat))
+        bin = convert.(IA.Interval{Float64}, IA.Interval.(brat, brat))
+
+        x, cert = epsilon_inflation(Ain, bin)
+
+        @test all(ones(n) .∈ x)
+        @test cert
+
+        # big float test
+        Abig = BigFloat.(Arat)
+        bbig = BigFloat.(brat)
+
+        x, cert = epsilon_inflation(Abig, bbig)
+
+        @test cert
+        @test all(diam.(x) .< 1e-50)
+        @test all(ones(n) .∈ x)
+
+        # case when should not be possible to certify
+        A = [1..2 1..4;0..1 0..1]
+        b = A*[1; 1]
+
+        _, cert = epsilon_inflation(A, b)
+        @test !cert
     end
 end
