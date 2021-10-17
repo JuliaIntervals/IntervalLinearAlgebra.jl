2277ed3ae2e2096134185412dda6fe02a8db75d8
lucaferranti <lucaa.ferranti@gmail.com>
2021-10-17 16:03:19 +0300

    updated compatibility with IA and readded NonLinearOettliPrager

--- test/test_solvers/test_oettli_prager.jl
+++ test/test_solvers/test_oettli_prager.jl
@@ -3,12 +3,12 @@
     A = [2..4 -2..1; -1..2 2..4]
     b = [-2..2, -2..2]
 
-    #p = solve(A, b, NonLinearOettliPrager())
+    p = solve(A, b, NonLinearOettliPrager())
 
     polyhedra = solve(A, b, LinearOettliPrager())
 
     for pnt in [[-4, -3], [3, -4], [4, 3], [-3, 4]]
-        #@test any(pnt ∈ x for x in p.boundary)
+        @test any(pnt ∈ x for x in p.boundary)
         @test sum(pnt ∈ pol for pol in polyhedra) == 1
     end
 
