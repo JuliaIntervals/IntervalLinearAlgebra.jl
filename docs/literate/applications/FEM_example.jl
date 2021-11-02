e23694a82da6c6d4bea294dcc335e5cf3295b038
lucaferranti <49938764+lucaferranti@users.noreply.github.com>
2021-11-02 18:21:35 +0200

    Update docs/literate/applications/FEM_example.jl

--- docs/literate/applications/FEM_example.jl
+++ docs/literate/applications/FEM_example.jl
@@ -38,7 +38,7 @@
 # \cos(\alpha) & -\sin(\alpha) & 0 & 0 \\
 #  \sin(\alpha) & \cos(\alpha) &  0 & 0 \\
 #  0 & 0 &  \cos(\alpha) & -\sin(\alpha) \\
-#  0 & 0 &  \sin(alpha) & \cos(alpha)
+#  0 & 0 &  \sin(\alpha) & \cos(\alpha)
 # \end{matrix}
 # \right),
 # ```
