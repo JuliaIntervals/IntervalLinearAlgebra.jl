c92f9e85627dbad35d59d21db38cba8c8c4c6de8
jorgepz <jorgepz@fing.edu.uy>
2021-11-03 04:44:38 -0300

    testing change image format

--- docs/literate/applications/FEM_example.jl
+++ docs/literate/applications/FEM_example.jl
@@ -68,8 +68,9 @@ end
 #
 # A problem based on Example 4.1 from [[SKA06]](@ref) is considered. The following diagram shows the truss structure considered.
 #
-# ![](../assets/trussDiagram.png)
-#
+# ```@raw html
+# <img src="../assets/trussDiagram.svg" alt="truss example diagram" width="500"/>
+#```
 #
 # ### Case with fixed parameters
 #
