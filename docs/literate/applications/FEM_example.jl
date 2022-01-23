dcaf5bb3253474550a344d5b11adf5bdccedfe17
Jorge Pérez Zerpa <42485529+jorgepz@users.noreply.github.com>
2022-01-23 15:28:16 -0300

    a few changes in the FEM example (#119)
    * editing FEM text
    
    * temporarily disable tests
    
    * more changes
    
    * added more description on the continuum problem
    
    * reactivate tests
    
    * update title
    
    Co-authored-by: lucaferranti <49938764+lucaferranti@users.noreply.github.com>

--- docs/literate/applications/FEM_example.jl
+++ docs/literate/applications/FEM_example.jl
@@ -1,19 +1,20 @@
-# # A simple FEM application
-# The Finite Element Method is widely used to solve PDEs in Engineering applications and particularly in Structural Analysis problems [[BAT14]](@ref). The procedure consists discretizing the domain into _elements_ and assembly a system of balance equations. For linear problems, this system can be usually written as
+# # Application of Interval Linear Algebra to FEM analysis
+# The Finite Element Method is widely used to solve PDEs in Engineering applications and particularly in Structural Analysis problems [[BAT14]](@ref). The procedure consists in discretizing the domain into _elements_ and constructing (_assembling_) a system of balance equations. For linear problems, this system can be usually written as
 # ```math
 # K \cdot d = f
 # \qquad
-# K = \sum_e K_e
+# K = \sum_{e=1}^{n_e} K_e
 # ```
-# where $f$ is the vector of external loads, $K_e$ is the element stiffness matrix and $d$
+# where $n_e$ is the number of elements of the domain, $f$ is the vector of external loads, $K_e$ is the stiffness matrix of element $e$ in global coordinates, $K$ is the assembled stiffness matrix and $d$
 # is the vector of unknown displacements. This tutorial shows how IntervalLinearAlgebra can
 # be used to solve structural mechanics problems with uncertainty in the parameters. Particularly,
 # it highlights the importance of parametric interval linear systems.
 #
 # ## Simple truss structure
 #
-# A frequent and simple type of structures are _Truss structures_, which are formed by bars connected but not welded. Truss models are considered during the conceptual design of bridges or other structures.
+# A frequent and simple type of structures are _Truss structures_, which are formed by bars connected but not welded. Truss models are usually considered during the conceptual design of bridges or other structures.
 #
+# ### Stiffness equations
 # The stiffness matrix of a truss element in the local coordinate system is given by
 # ```math
 # K_L = s
@@ -27,7 +28,7 @@
 # \right),
 # ```
 #
-# where $s =\frac{E A}{L}$, with $E$ being the Young modulus, $A$ the area of the cross-section and $L$ the length of that truss element.
+# where $s =\frac{E A}{L}$ is the stiffness, $E$ is the Young modulus, $A$ is the area of the cross-section and $L$ is the length of that truss element.
 #
 # The change-of-basis matrix is given by
 # ```math
@@ -39,17 +40,18 @@
 #  0 & 0 &  \cos(\alpha) & -\sin(\alpha) \\
 #  0 & 0 &  \sin(\alpha) & \cos(\alpha)
 # \end{matrix}
-# \right),
+# \right).
 # ```
 #
 # The system of equations for each element is written in local coordinates as
 # ```math
 # K_L d_L = f_L
 # ```
-# and using the change-of-basis we obtain
+# and using the change-of-basis we obtain the equations for that element in the global systems of coordinates
 # ```math
-# K_G d_G = f_G \qquad K_G = Q K_L Q^T
+# K_G d_G = f_G \qquad K_G = Q K_L Q^T.
 # ```
+# After the system of equations for each element is in global coordinates, the whole system is assembled.
 #
 # The unitary stiffness matrix (for $s=1$) can be computed using the following function.
 function unitaryStiffnessMatrix( coordFirstNode, coordSecondNode  )
@@ -71,7 +73,7 @@ end
 # <img src="../../assets/trussDiagram.svg" style="width: 100%" alt="truss diagram"/>
 # ```
 #
-# ### Case with fixed parameters
+# #### Case with fixed parameters
 #
 # The scalar parameters considered are given by
 E = 2e11 ; # Young modulus
@@ -89,11 +91,11 @@ connecMatrix = [ 1     2 ;
                  2     4 ;
                  3     4 ;
                  3     5 ;
-                 4     5 ]
+                 4     5 ];
 # and the fixed degrees of freedom (supports) are defined by the vector
-fixedDofs = [2 9 10 ]
+fixedDofs = [ 2 9 10 ];
 
-# calculations
+# The number of elements and nodes are computed, as well as the free degrees of freedom.
 numNodes = size( nodesCMatrix )[1]  # compute the number of nodes
 numElems = size( connecMatrix )[1]  # compute the number of elements
 freeDofs = zeros(Int8, 2*numNodes-length(fixedDofs))
@@ -106,7 +108,7 @@ while indDof <= (2*numNodes)
   global indDof = indDof + 1
 end
 
-# assembly
+# The global stiffness equations are computed for the unknown displacements (free dofs)
 KG = zeros( 2*numNodes, 2*numNodes )
 FG = zeros( 2*numNodes )
 for elem in 1:numElems
@@ -125,12 +127,12 @@ FG[4] = -1e4 ;
 KG = KG[ freeDofs, : ]
 KG = KG[ :, freeDofs ]
 FG = FG[ freeDofs ]
-
+# and the system is solved.
 u = KG \ FG
 UG = zeros( 2*numNodes )
 UG[ freeDofs ] = u
 #
-# The reference (dashed blue line) and deformed (solid red)  configurations of the structure are ploted.
+# The reference (dashed blue line) and deformed (solid red) configurations of the structure are ploted.
 # Since the displacements are very small, a `scaleFactor` is considered to amplify
 # the deformation and ease the visualization.
 #
@@ -160,7 +162,7 @@ savefig("deformed.png") # hide
 #
 # ![](deformed.png)
 #
-# ### Problem with interval parameters
+# #### Problem with interval parameters
 
 md"""
 Suppose now we have a 10% uncertainty for the stiffness $s_{23}$ associated with the third
@@ -203,12 +205,12 @@ KGp = AffineParametricArray(KGp)
 srange = E * A / sqrt(2) ± 0.1 * E * A / sqrt(2)
 
 # To solve the system, we could of course just subsitute `srange` into the parametric matrix
-# `KGp` and solve the "normal" interval linear system
+# `KGp` and solve the "normal" interval linear system (naive approach)
 
 usimple = solve(KGp(srange), Interval.(FG))
 
-# This approach, however soffers from the [dependency problem](https://en.wikipedia.org/wiki/Interval_arithmetic#Dependency_problem)
-# and hence the compute displacement will be an overestimation of the true displacement.
+# This approach, however suffers from the [dependency problem](https://en.wikipedia.org/wiki/Interval_arithmetic#Dependency_problem)
+# and hence the computed displacements will be an overestimation of the true displacements.
 
 # To mitigate this issue, algorithms to solve linear systems with parameters have been developed.
 # In this case we use the algorithm presented in [[SKA06]](@ref)
@@ -228,9 +230,11 @@ next section.
 """
 # ## A continuum mechanics problem
 
-# TODO: GENERAL DESCRIPTION OF THE PROBLEM
+md"""
+In this problem a simple solid plane problem is considered. The solid is fixed on its bottom edge and loaded with a shear tension on the top edge.
+"""
 
-# First, we define and cosntruct the mesh
+# First, we set the geometry and construct a regular grid of points
 
 L   = [1.0, 4.0]         # dimension in each direction
 t   = 0.2                # thickness
@@ -240,7 +244,9 @@ nel = [nx, ny]
 neltot = 2 * nx * ny;    # total number of elements
 nnos         = nel .+ 1       # number of nodes in each direction
 nnosx, nnosy = nnos
-nnostot      = nnosx * nnosy  # total number of nodes
+nnostot      = nnosx * nnosy ;  # total number of nodes
+
+# we compute the vector of indexes of the loaded nodes (bottom ones)
 
 startloadnode = (nnosy - 1) * nnosx + 1 # boundary conditions
 endinloadnode = nnosx * nnosy
@@ -248,13 +254,16 @@ LoadNodes = startloadnode:endinloadnode
 lins1   = range(0, L[1], length=nnosx)
 lins2   = range(0, L[2], length=nnosy)
 
-nodes = zeros(nnostot, 2) # nodes: first column x-coord, second column y-coord
+# and construct the matrix of coordinates of the nodes
 
+nodes = zeros(nnostot, 2) # nodes: first column x-coord, second column y-coord
 for i = 1:nnosy   # first discretize along y-coord
     idx = (nnosx * (i-1) + 1) : (nnosx*i)
     nodes[idx, 1] = lins1
     nodes[idx, 2] = fill(lins2[i], nnosx)
 end
+
+# The connectivity matrix Mcon is computed, considering 3-node triangular elements
 
 Mcon = Matrix{Int64}(undef, neltot, 3); # connectivity matrix
 for j = 1:ny
@@ -266,16 +275,14 @@ for j = 1:ny
     end
 end
 
-Xel = Matrix{Float64}(undef, 3, neltot)
-Yel = Matrix{Float64}(undef, 3, neltot)
+# the undeformed mesh is plotted as follows
 
+Xel = Matrix{Float64}(undef, 3, neltot); Yel = Matrix{Float64}(undef, 3, neltot)
 for i = 1:neltot
     Xel[:, i] = nodes[Mcon[i, :], 1] # the j-th column has the x value at the j-th element
     Yel[:, i] = nodes[Mcon[i, :], 2] # the j-th column has the y value at the j-th element
 end
-
 fig = plot(ratio=1, xlimits=(-1, 3), title="Undeformed mesh", xlabel="x", ylabel="y")
-
 plot!(fig, [Xel[:, 1]; Xel[1, 1]], [Yel[:, 1]; Yel[1, 1]], linecolor=:blue, linewidth=1.4, label="")
 for i = 2:neltot
     plot!(fig, [Xel[:, i]; Xel[1, i]], [Yel[:, i]; Yel[1, i]], linecolor=:blue, linewidth=1.4, label="")
@@ -284,49 +291,45 @@ savefig("undeformed2.png") # hide
 
 # ![](undeformed2.png)
 
-# Let us now define the material parameters. Here we assume a 10% uncertainty on the Young modulus
+# Let us now define the material parameters. Here we assume a 10% uncertainty on the Young modulus, while Poisson ratio and the density are fixed. This can be related to a steel plate problem with an unknown composition, thus unknown exact Young modulus value.
 
-ν  = 0.25   # Poisson modulus
+ν  = 0.25   # Poisson
 ρ  = 8e3  # density
 
 @affinevars E # Young modulus, defined as symbolic variable
 En = 200e9  # nominal value of the young modulus
 Erange = En ± 0.1 * En # uncertainty range of the young modulus
 
-# We can now assemble the stiffness matrix
-
-## matrix for plain stress state
+# We can now assemble the global stiffness matrix.
+# We set the constitutive matrix for a plane stress state.
 C = E / (1-ν^2) * [ 1    ν         0 ;
                     ν    1         0 ;
                     0    0   (1-ν)/2 ]
 
-
-# Degrees of freedom (fixed and free)
+# We compute the free and fixed degrees of freedom
 function nodes2dofs(u)
     v = Vector{Int64}(undef, 2*length(u))
     for i in 1:length(u)
-        v[2i-1] = 2u[i] - 1
-        v[2i] = 2u[i]
+        v[2i-1] = 2u[i] - 1;   v[2i] = 2u[i]
     end
     return v
 end
-
 FixNodes = 1:nnosx
 FixDofs = nodes2dofs(FixNodes)              # first add all dofs of the nodes
 deleteat!(FixDofs, 3:2:(length(FixDofs)-2)) # then remove the free dofs of the nodes
-
 LibDofs = Vector(1:2*nnostot)               # free degrees of fredom
 deleteat!(LibDofs, FixDofs)
+
+# and we assemble the matrix
 
 function stiffness_matrix(x, y, C, t)
 
     A = det([ones(1, 3); x'; y']) / 2 # element area
-
     B = 1 / (2*A) * [y[2]-y[3]                  0    y[3]-y[1]             0    y[1]-y[2]           0   ;
                              0          x[3]-x[2]            0     x[1]-x[3]            0     x[2]-x[1] ;
                      x[3]-x[2]          y[2]-y[3]    x[1]-x[3]     y[3]-y[1]    x[2]-x[1]     y[1]-y[2] ]
 
-    K = B' * C * B * A * t
+    K = B' * C * B * A * t ;
     return K
 end
 
@@ -339,7 +342,7 @@ end
 
 K = AffineParametricArray(KG[LibDofs, LibDofs])
 
-# Finally, we define the load
+# Finally, we assemble the loads vector
 
 areaelemsup = L[1] / nx * t
 
@@ -364,7 +367,7 @@ nothing # hide
 
 u = solve(K, FLib, Erange) # solving
 
-## plotting
+# plotting
 U = zeros(Interval, 2*nnostot)
 U[LibDofs] .= u
 
@@ -376,7 +379,7 @@ nodesdef = hcat(nodes[:, 1] + Ux, nodes[:, 2] + Uy);
 Xeld = Interval.(copy(Xel))
 Yeld = Interval.(copy(Yel))
 
-## build elements coordinate vectors
+# build elements coordinate vectors
 for i = 1:neltot
     Xeld[:, i] = nodesdef[Mcon[i, :], 1] #  the j-th column has the x coordinate of the j-th element
     Yeld[:, i] = nodesdef[Mcon[i, :], 2] #  the j-th column has the y coordinate of the j-th element
