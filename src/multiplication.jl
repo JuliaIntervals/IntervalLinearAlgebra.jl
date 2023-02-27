3f74d223820990eca05bddc42cdb85fb1ad59ce1
Isaia Nisoli <isaia.nisoli@gmail.com>
2023-02-27 16:10:24 +0900

    Fix complex matrix multiplication (#133)
    * Added methods to currectly dispatch complex mul
    
    * Added methods to dispatch correctly cpx matrix mul

--- src/multiplication.jl
+++ src/multiplication.jl
@@ -38,6 +38,29 @@ function set_multiplication_mode(multype)
     config[:multiplication] = multype
 end
 
+function *(A::AbstractMatrix{Complex{Interval{T}}}, B::AbstractMatrix) where T
+    return real(A)*B+im*imag(A)*B
+end
+
+function *(A::AbstractMatrix, B::AbstractMatrix{Complex{Interval{T}}}) where T
+    return A*real(B)+im*A*imag(B)
+end
+
+function *(A::AbstractMatrix{Complex{Interval{T}}}, B::AbstractMatrix{Complex{Interval{T}}}) where T
+    rA, iA = real(A), imag(A)
+    rB, iB = real(B), imag(B)
+    return rA*rB-iA*iB+im*(iA*rB+rA*iB)
+end
+
+function *(A::AbstractMatrix{Complex{T}}, B::AbstractMatrix{Interval{T}}) where {T}
+    return real(A)*B+im*imag(A)*B
+end
+
+function *(A::AbstractMatrix{Interval{T}}, B::AbstractMatrix{Complex{T}}) where {T}
+    return A*real(B)+im*A*imag(B)
+end
+
+
 function *(::MultiplicationType{:slow}, A, B)
     TS = promote_type(eltype(A), eltype(B))
     return mul!(similar(B, TS, (size(A,1), size(B,2))), A, B)
