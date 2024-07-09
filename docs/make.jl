77156c3827ca3fd84a28d17d9e6f1ccf731fdb5a
lucaferranti <lucaa.ferranti@gmail.com>
2024-07-09 11:08:38 +0300

    dont run documenter in strict mode

--- docs/make.jl
+++ docs/make.jl
@@ -22,6 +22,7 @@ makedocs(;
     authors="Luca Ferranti",
     repo="https://github.com/JuliaIntervals/IntervalLinearAlgebra.jl/blob/{commit}{path}#{line}",
     sitename="IntervalLinearAlgebra.jl",
+    strict=false,
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://juliaintervals.github.io/IntervalLinearAlgebra.jl",
